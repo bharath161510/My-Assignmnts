bc556f55da62eecc06ddb1f5e0440747ffaf49a4
bharath161510 <164643368+bharath161510@users.noreply.github.com>
2024-03-23 19:20:37 +0530

    Add files via upload
    Week1.Day1

---
+++ day1/Bike.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Bike {
+	public static void main(String[] args) {
+		
+		Car1 myCar = new Car1();
+		myCar.applyBreak();
+		myCar.soundHorn();
+	}
+
+}
---
+++ day1/Car1.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Car1 {
+	public void  applyBreak () {	
+	
+		System.out.println("Applied Brake");
+	}
+
+	public void soundHorn () {
+		
+		System.out.println("Sound Horn");
+	}
+}
