1559fee3d6041817a93d6d330e451558fe43f986
bharath161510 <164643368+bharath161510@users.noreply.github.com>
2024-03-25 11:08:08 +0530

    Add files via upload
    week1.day2

---
+++ day2/NegtoPostive.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class NegtoPostive {
+	public static void main(String[] args) {
+		int number = -40;
+			System.out.println(Math.abs(number));
+	}
+}
+
+//Math.abs is a method through which we can obtain only positive values, 
+//even though we give negative values.
---
+++ day2/PostNegt.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class PostNegt {
+	public static void main(String[] args) {
+		int number = 35;
+		if (number<0) {
+			System.out.println("The given number is a Negative Number");
+		}
+		else {
+			System.out.println("The given number is Positive Number");
+		}
+	}
+
+}
